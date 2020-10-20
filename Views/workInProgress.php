3b35654da657552943cf10cb9391f47b560c2195
ecettour <ecettour@emmsa.net>
2020-10-20 16:49:13 -0300

    Agrego controllers pendientes y una vista de WIP para que no tire error al intentar acceder.

---
+++ Views/workInProgress.php
@@ -0,0 +1,8 @@
+<?php
+include_once(VIEWS_PATH."nav.php");
+?>
+<img src="<?=IMG_PATH?>WIP.jpg" alt="Work in Progress!" style="width:100%;">
+
+<script type="text/javascript">    
+alert("We're currently working at this feature (?");
+</script>
