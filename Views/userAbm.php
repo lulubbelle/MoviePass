711db755dd125b0b6fade8ab2bf938de611f1f9b
lulubbelle <luli.icasatti@gmail.com>
2020-11-08 16:43:55 -0300

    views de User

--- Views/userAbm.php
+++ Views/userAbm.php
@@ -11,39 +11,43 @@
                     <h1 class="basic-font cinema-view-title">Mis Datos</h1>                
                 </div>
             <!-- form -->
-            <div class="col-md-10">
-                <form action="<?php echo FRONT_ROOT ?>Profile/UpdateUser" method="post">
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label class="cinema-input-label" for="inputEmail">Email</label>
-                            <input type="email" class="form-control" value="<?php echo ($profile->getMail()) ?>" id="inputEmail" placeholder="Email" disabled>
+            <div class="col-md-12">
+                <div class="form-profile">
+
+                    <form  action="<?php echo FRONT_ROOT ?>Profile/UpdateUser" method="post">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label class="cinema-input-label" for="inputEmail">Email</label>
+                                <input type="email" class="form-control" value="<?php echo ($profile->getMail()) ?>" id="inputEmail" placeholder="Email" disabled>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label class="cinema-input-label" for="inputNombre"><i style="color: red;">&#42&nbsp</i>Nombre de Usuario</label>
+                                <input type="text" name="user_name" value="<?php echo ($profile->getUserName()) ?>" class="form-control" id="inputNombre" placeholder="Nombre" required>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label class="cinema-input-label" for="inputNombre"><i style="color: red;">&#42&nbsp</i>Nombre</label>
+                                <input type="text" name="first_name" value="<?php echo ($profile->getFirstName()) ?>" class="form-control" id="inputNombre" placeholder="Nombre" required>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label class="cinema-input-label" for="inputNombre"><i style="color: red;">&#42&nbsp</i>Apellido</label>
+                                <input type="text" name="last_name" value="<?php echo ($profile->getLastName()) ?>" class="form-control" id="inputNombre" placeholder="Apellido" required>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label class="cinema-input-label" for="inputNombre"><i style="color: red;">&#42&nbsp</i>DNI</label>
+                                <input type="text" name="dni" value="<?php echo ($profile->getDNI()) ?>" class="form-control" id="inputNombre" placeholder="DNI" required>
+                            </div>
+                            <div class="form-group col-md-6" style="display:none">
+                                <input type="text" name="user_id" value="<?php echo ($profile->getUserId()) ?>" class="form-control" id="inputId" placeholder="Id" required>
+                            </div>
+                            <div class="form-group col-md-6" style="display:none">
+                                <input type="text" name="user_profile_id" value="<?php echo ($profile->getId()) ?>" class="form-control" id="upId" placeholder="upId" required>
+                            </div>
                         </div>
-                        <div class="form-group col-md-6">
-                            <label class="cinema-input-label" for="inputNombre"><i style="color: red;">&#42&nbsp</i>Nombre de Usuario</label>
-                            <input type="text" name="user_name" value="<?php echo ($profile->getUserName()) ?>" class="form-control" id="inputNombre" placeholder="Nombre" required>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label class="cinema-input-label" for="inputNombre"><i style="color: red;">&#42&nbsp</i>Nombre</label>
-                            <input type="text" name="first_name" value="<?php echo ($profile->getFirstName()) ?>" class="form-control" id="inputNombre" placeholder="Nombre" required>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label class="cinema-input-label" for="inputNombre"><i style="color: red;">&#42&nbsp</i>Apellido</label>
-                            <input type="text" name="last_name" value="<?php echo ($profile->getLastName()) ?>" class="form-control" id="inputNombre" placeholder="Apellido" required>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label class="cinema-input-label" for="inputNombre"><i style="color: red;">&#42&nbsp</i>DNI</label>
-                            <input type="text" name="dni" value="<?php echo ($profile->getDNI()) ?>" class="form-control" id="inputNombre" placeholder="DNI" required>
-                        </div>
-                        <div class="form-group col-md-6" style="display:none">
-                            <input type="text" name="user_id" value="<?php echo ($profile->getUserId()) ?>" class="form-control" id="inputId" placeholder="Id" required>
-                        </div>
-                    </div>
-    
-                    
-                    
-                    <button type="submit" class="btn btn-block cinema-btn"><i class="fas fa-save"></i>&nbspGuardar cambios</button>
-                    <a href="<?php echo FRONT_ROOT ?>Profile/" class="btn btn-primary"><i class="fas fa-arrow-left"></i>&nbspVolver</a>
-                </form>
+                                                                        
+                        <button type="submit" class="btn btn-block cinema-btn"><i class="fas fa-save"></i>&nbspGuardar cambios</button>
+                        <a href="<?php echo FRONT_ROOT ?>Profile/" class="btn btn-primary btn-volver"><i class="fas fa-arrow-left"></i>&nbspVolver</a>
+                    </form>
+                </div>
                 <!-- form -->
             </div>
         </div>
@@ -56,27 +60,12 @@
 </html>
 
 <style>
-    #box {
-        margin-top: 2%;
-        min-height: 85vh !important;
-        border-radius: 25px;
+    .form-profile {
+        margin: 0 auto;
+        width: 70%;
     }
 
-    #imgProfile {
-        border-radius: 50%;
-        border: 4px solid black;
-        background-color: rgba(52, 57, 64, 0.5);
-    }
-
-    .imgOption {
-        border-radius: 50%;
-        border: 4px solid black;
-        background-color: rgba(52, 57, 64, 0.5);
-    }
-
-    .imgButton{
-        background-color: transparent;
-        border: none;
-        margin-bottom: 5%;
+    .btn-volver {
+        margin-bottom: 10%;
     }
 </style>
--- Views/userView.php
+++ Views/userView.php
@@ -1,24 +1,29 @@
 <?php require_once(VIEWS_PATH."nav.php"); ?>
 <link rel="stylesheet" href="<?= CSS_PATH ?>/user.css">
 
-<div class="card-container">
 <div class="row">
+<div class="card-container">
         <div class="col-md-12">
     <img
-        class="round"
+        class="img-profile"
         src="https://randomuser.me/api/portraits/women/79.jpg"
         alt="user"
     />
-    <h3>Bienvenido <?php echo $user->getUserName() ?></h3>
-    <h3>Email: <?php echo $user->getMail() ?></h3>
-    <h3>Usuario: <?php echo $user->getUserName() ?></h3>
-    <h3>nombre: <?php echo $profile->getFirstName() ?></h3>
-    <h3>Apellido: <?php echo $profile->getLastName() ?></h3>
-    <h3>DNI: <?php echo $profile->getDNI() ?></h3>
+    <?php if(isset($user)) {?>
+        <h1 class="profile-title">Bienvenido <?php echo $user->getUserName() ?></h3>
+        <div class="text-container">
+            <h3 class="profile-text">Email: <span class="profile-info"><?php echo $user->getMail() ?></span></h3>
+            <h3 class="profile-text">Usuario: <span class="profile-info"><?php echo $user->getUserName() ?></span></h3>
+            <?php if(isset($profile)) {?>
+            <h3 class="profile-text">Nombre: <span class="profile-info"><?php echo $profile->getFirstName() ?></span></h3>
+            <h3 class="profile-text">Apellido: <span class="profile-info"><?php echo $profile->getLastName() ?></span></h3>
+            <h3 class="profile-text">DNI: <span class="profile-info"><?php echo $profile->getDNI() ?></span></h3>
 
-  
-    <a href="<?php echo FRONT_ROOT ?>Profile/UpdateUserShowView" class="btn btn-success"><i class="fas fa-save"></i>&nbspModificar Datos</a>
+            <a href="<?php echo FRONT_ROOT ?>Profile/UpdateUserShowView" class="btn btn-success btn-block btn-profile"><i class="fas fa-save"></i>&nbspModificar Datos</a>
+        </div>
+        <?php }
+        }?>
+
     </div>
     </div>
 </div>
-
